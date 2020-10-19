fd19eb0760616d52bb9f93cae0dc6f2226440640
PhilAtKitronik <phil.bradwell@kitronik.co.uk>
2020-10-19 16:08:34 +0100

    Added library for the sensors of MOVE Motor

---
+++ KitronikMOVEMotorSensors.py
@@ -0,0 +1,51 @@
+# microbit-module: KitronikMOVEMotorSensors@1.0.0
+# Copyright (c) Kitronik Ltd 2019. 
+#
+# The MIT License (MIT)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from microbit import i2c, pin13, pin14
+import machine
+import math
+import utime
+
+class MOVEMotorSensors:
+
+    def distanceCm(self):
+        pin14.set_pull(pin14.NO_PULL)
+        pin13.write_digital(0)
+        utime.sleep_us(2)
+        pin13.write_digital(1)
+        utime.sleep_us(10)
+        pin13.write_digital(0)             
+        distance = machine.time_pulse_us(pin14,1,1160000)
+        if distance>0:
+            return round(distance/58)
+        else:
+            return round(distance)
+
+    def distanceInch(self):
+        return (self.distanceCm() * 0.3937)
+
+    def readLineFollow(self, sensor):
+        if sensor == "left":
+            return pin2.read_analog()
+        elif sensor == "right":
+            return pin1.read_analog()
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# micropython-microbit-kitronik-MOVE-motor-sensors
+# Kitronik example micropython for micro:bit code 
+# micropython-microbit-kitronik-MOVE-motor-sensor
+Sample code for the [:MOVE Motor](https://www.kitronik.co.uk/5683) in micro python. Uses a module for reading distance and line following sensors. 
+
+## License
+
+MIT
+
+## Supported targets
+micropython/microbit
